9f663640913b642a1ea081f3ff99678359bd4317
Adatix <rathi.ady@gmail.com>
2022-04-23 16:12:25 +0530

    added putput facility

--- getFiles.py
+++ getFiles.py
@@ -16,6 +16,7 @@ Purchase_regex = '.*PURCHASE_.*.gz$'
 GenericNonSource_regex = '.*GENERIC_NONSOURCE_.*.gz$'
 GenericSource_regex = '.*GENERIC_SOURCE_.*.gz$'
 filePath = ini_parser.get('FILE_OSS','path')
+outputPath = ini_parser.get('FILE_OSS','outputpath')
 listFilesAtPath = [filePath+"\\"+fd for fd in os.listdir(filePath)]
 fileBrandPurchase = ''
 filePurchase = ''
@@ -32,33 +33,35 @@ for file in listFilesAtPath:
     if re.match(GenericSource_regex, file):
         fileGenericSource = file
 
-print(fileBrandPurchase)
+# print(fileBrandPurchase)
 
 df_fileBrandPurchase = pd.read_csv(fileBrandPurchase, compression='gzip', header=0, sep='\t')
 df_fileBrandPurchaseMod = df_fileBrandPurchase[['PurchaseDate','PurchaseNumber']].copy()
 df_fileBrandPurchaseMod['PurchaseDate'] = pd.to_datetime(df_fileBrandPurchaseMod['PurchaseDate'], errors='coerce')
 df_fileBrandPurchaseModAgg= df_fileBrandPurchaseMod.groupby(df_fileBrandPurchaseMod['PurchaseDate'].dt.date)['PurchaseNumber'].agg(['count']).reset_index().rename(columns={'PurchaseDate':'Brand_PurchaseFile_Date', 'count': 'CountofBrand_PurchaseNumbers'})
+df_fileBrandPurchaseModAgg.to_csv(outputPath+'/Brand_Purchase_log.txt',sep='\t')
 
 df_filePurchase = pd.read_csv(filePurchase, compression='gzip', header=0, sep='\t')
 df_filePurchaseMod = df_filePurchase[['PurchaseDate','PurchaseNumber']].copy()
 df_filePurchaseMod['PurchaseDate'] = pd.to_datetime(df_filePurchaseMod['PurchaseDate'], errors='coerce')
 df_filePurchaseModAgg= df_filePurchaseMod.groupby(df_filePurchaseMod['PurchaseDate'].dt.date)['PurchaseNumber'].agg(['count']).reset_index().rename(columns={'PurchaseDate':'PurchaseFile_Date', 'count': 'CountofPurchaseNumbers'})
-
+df_filePurchaseModAgg.to_csv(outputPath+'/Purchase_log.txt',sep='\t')
 
 df_fileGenericNonSource = pd.read_csv(fileGenericNonSource, compression='gzip', header=0, sep='\t')
 df_fileGenericNonSourceMod = df_fileGenericNonSource[['Purchase_Date','PurchaseUID']].copy()
 df_fileGenericNonSourceMod['Purchase_Date'] = pd.to_datetime(df_fileGenericNonSourceMod['Purchase_Date'], errors='coerce')
 df_fileGenericNonSourceModAgg= df_fileGenericNonSourceMod.groupby(df_fileGenericNonSourceMod['Purchase_Date'].dt.date)['PurchaseUID'].agg(['count']).reset_index().rename(columns={'Purchase_Date':'GenericNonSourceFile_Date', 'count': 'CountofPGenericNonSource'})
-
+df_fileGenericNonSourceModAgg.to_csv(outputPath+'/GenericNonSource_log.txt',sep='\t')
 
 df_fileGenericSource = pd.read_csv(fileGenericSource, compression='gzip', header=0, sep='\t')
 df_fileGenericSourceMod = df_fileGenericSource[['PurchaseDate','PurchaseUID']].copy()
 df_fileGenericSourceMod['PurchaseDate'] = pd.to_datetime(df_fileGenericSourceMod['PurchaseDate'], errors='coerce')
-df_fileGenericSourceModAgg= df_fileGenericSourceMod.groupby(df_fileGenericSourceMod['PurchaseDate'].d.date)['PurchaseUID'].agg(['count']).reset_index().rename(columns={'PurchaseDate':'GenericSourceFile_Date', 'count': 'CountofGenericSource'})
+df_fileGenericSourceModAgg= df_fileGenericSourceMod.groupby(df_fileGenericSourceMod['PurchaseDate'].dt.date)['PurchaseUID'].agg(['count']).reset_index().rename(columns={'PurchaseDate':'GenericSourceFile_Date', 'count': 'CountofGenericSource'})
+df_fileGenericSourceModAgg.to_csv(outputPath+'/GenericSource_log.txt',sep='\t')
 
 
 
-print(df_fileBrandPurchaseModAgg)
-print(df_fileGenericNonSourceModAgg)
-print(df_fileGenericSourceModAgg)
-print(df_filePurchaseModAgg)
+# print(df_fileBrandPurchaseModAgg)
+# print(df_fileGenericNonSourceModAgg)
+# print(df_fileGenericSourceModAgg)
+# print(df_filePurchaseModAgg)
